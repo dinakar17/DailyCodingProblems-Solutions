da0f4a64a08fca1e95846abacc3f55f97872ebc3
dinakar17 <dinakarchennupati777@gmail.com>
2022-07-14 09:40:36 +0530

    Amazon - Assign Mice to Holes

---
+++ #324.cpp
@@ -0,0 +1,25 @@
+/*
+This problem was asked by Amazon.
+
+Consider the following scenario: there are N mice and N holes placed at integer points along a line. Given this, find a method that maps mice to holes such that the largest number of steps any mouse takes is minimized.
+
+Each move consists of moving one mouse one unit to the left or right, and only one mouse can fit inside each hole.
+
+For example, suppose the mice are positioned at [1, 4, 9, 15], and the holes are located at [10, -5, 0, 16]. In this case, the best pairing would require us to send the mouse at 1 to the hole at -5, so our function should return 6.
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int MiceToHoles(vector<int>& mice, vector<int>& holes){
+    sort(mice.begin(), mice.end());
+    sort(holes.begin(), holes.end());
+
+    int n = mice.size();
+    int max_time = 0;
+    for(int i = 0; i < n; i++){
+        max_time = max(max_time, abs(mice[i] - holes[i]));
+    }
+
+    return max_time;
+}
